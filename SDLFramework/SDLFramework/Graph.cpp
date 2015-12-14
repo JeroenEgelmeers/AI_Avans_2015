213fda00c698209ca7f97a991de52e7589d55617
rhsattev <rhs.vanatteveld@student.avans.nl>
2015-12-14 22:33:00 +0100

    printing line values

--- SDLFramework/SDLFramework/Graph.cpp
+++ SDLFramework/SDLFramework/Graph.cpp
@@ -46,6 +46,7 @@ void Graph::Draw() {
 		Node b = nodes[e.GetSecond()];
 
 		mApplication->DrawLine(a.x, a.y, b.x, b.y);
+		mApplication->DrawText(std::to_string(e.GetLength()), (a.x + b.x) / 2, (a.y + b.y) / 2);
 	}
 
 	mApplication->SetColor(Color(255, 0, 0, 255));
@@ -54,7 +55,7 @@ void Graph::Draw() {
 	for (auto &n : nodes)
 	{
 		mApplication->DrawRect(n.x - 5, n.y - 5, 10, 10, true);
-		mApplication->DrawText(std::to_string(count), n.x - 20, n.y + 50);
+		mApplication->DrawText(std::to_string(count), n.x - 50, n.y + 10);
 		++count;
 	}
 
